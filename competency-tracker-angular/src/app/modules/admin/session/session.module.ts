c41edf50bc532b6b1053cf00c358da96f88f6a17
gowthamkoffcl <gowthamkoffcl@gmail.com>
2025-08-30 15:01:47 +0530

    mistaken instead of maven i imported groovy

--- competency-tracker-angular/src/app/modules/admin/session/session.module.ts
+++ competency-tracker-angular/src/app/modules/admin/session/session.module.ts
@@ -5,12 +5,14 @@ import { SessionRoutingModule } from './session-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SharedModule } from '../../../shared/shared.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     CommonModule,
